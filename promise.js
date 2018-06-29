adef2fa279c442aae4c4389a613dd5c2ee4b1523
Raoul1996 <baobaozhou199606@gmail.com>
2018-06-29 23:51:10 +0800

    complete the promise
    Change-Id: Ia8d414cf13cd21826f81899ce590e71f6819b029

--- promise.js
+++ promise.js
@@ -21,25 +21,133 @@ class Promise {
     };
     try {
       executor(resolve, reject)
-    } catch (e) {
-      reject(e);
+    } catch (err) {
+      reject(err);
     }
   }
 
   then(onFulfilled, onRejected) {
-    if (this.state === 'fulfilled') {
-      onFulfilled(this.value);
-    }
-    if (this.state === 'rejected') {
-      onRejected(this.reason);
-    }
-    if (this.state === 'pending') {
-      this.onResolvedCallbacks.push(() => {
-        onFulfilled(this.value);
-      });
-      this.onRejectedCallbacks.push(() => {
-        onRejected(this.value);
-      });
-    }
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected : err => {
+      throw err
+    };
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.state === 'fulfilled') {
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      }
+      if (this.state === 'rejected') {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      }
+      if (this.state === 'pending') {
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+      }
+    });
+    return promise2;
   }
 }
+
+function resolvePromise(promise2, x, resolve, reject) {
+  if (x === promise2) {
+    return reject(new TypeError('Chaining cycle detected for promise'));
+  }
+  let called;
+  if (x != null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      let then = x.then;
+      if (typeof then === 'function') {
+        then.call(x, y => {
+          if (called) return;
+          called = true;
+          resolvePromise(promise2, y, resolve, reject)
+        }, err => {
+          if (called) return;
+          called = true;
+          reject(err);
+        });
+      } else {
+        resolve(x);
+      }
+    } catch (e) {
+      if (called) return;
+      called = true;
+      reject(e);
+    }
+
+  } else {
+    resolve(x);
+  }
+}
+
+Promise.resolve = function(val) {
+  return new Promise((resolve, reject) => {
+    resolve(val);
+  });
+};
+
+Promise.reject = function(val) {
+  return new Promise((resolve, reject) => {
+    reject(val);
+  });
+};
+
+Promise.race = function(promises) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(resolve, reject)
+    }
+  });
+};
+
+Promise.all = function(promises) {
+  let arr = [];
+  let i = 0;
+
+  function processData(index, data) {
+    arr[index] = data;
+    i++;
+    if (i === promises.length) {
+      resolve(arr);
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(data => {
+        processData(i, data);
+      }, reject);
+    }
+  });
+};
